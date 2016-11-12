e2719f4b8f6f5ee041d915b88a050520a93159dc
Carmen Barron <carmen.barron@gmail.com>
2016-11-11 22:14:21 -0500

    add wording for themes, add route for inserting user input to theme stories

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,11 +1,12 @@
 class DefaultController < ApplicationController
 
-  def homepage
-
+  def home
   end
 
   def finished_story
-
+    @params = params
+    # @something = params[:noun1]
+    # @something = "test dudette"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/', to: 'application#homepage'
+  # get '/', to: 'application#home'
 
-  get 'home' => 'default#homepage'
+  get 'home' => 'default#home'
 
   get 'finished_story' =>'default#finished_story'
 
   # post 'home' => 'dictionary#submit_ten_words'
 
-  post 'submit_ten_words' => 'dictionary#submit_ten_words'
+  post 'submit_ten_words' => 'dictionary#submit_ten_wordsies'
 
 
 
